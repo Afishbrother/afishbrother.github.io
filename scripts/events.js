f8ba8722a11445be5f1b4b616ed356be3d6504f8
Afishbrother <48510702+Afishbrother@users.noreply.github.com>
2020-07-09 12:02:53 +0800

    add loop eventlistener
    增加循环监听

--- scripts/events.js
+++ scripts/events.js
@@ -2,6 +2,15 @@ var hanbing = document.getElementById("hanbing");
 var liulang = document.getElementById("liulang");
 var jiansheng = document.getElementById("jiansheng");
 var hideall = document.getElementById("hideall");
+var piclist = document.getElementById("piclist"); //one result
+var opts= piclist.querySelectorAll("li"); // four results
+
+// console.log(hanbing)
+// var a =opts[0].querySelector("a");
+// console.log(a);
+
+//console.log(piclist);
+//console.log(opts);
 // var hp = document.getElementById("hanbing-pic"); 
 // var lp = document.getElementById("liulang-pic"); 
 // var jp = document.getElementById("jiansheng-pic"); 
@@ -31,8 +40,13 @@ function hide(){
 
 }
 
-hanbing.addEventListener("click",showpic);
-liulang.addEventListener("click", showpic);
-jiansheng.addEventListener("click", showpic);
+for (var i =0; i<opts.length-1; i++){
+    var alink = opts[i].querySelector("a");
+    //console.log(alink);
+    alink.addEventListener("click",showpic);
+}
+// hanbing.addEventListener("click",showpic);
+// liulang.addEventListener("click", showpic);
+// jiansheng.addEventListener("click", showpic);
 hideall.addEventListener("click",hide);
 
